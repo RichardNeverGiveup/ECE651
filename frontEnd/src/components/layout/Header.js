8e7727836260ac64bd3ab63bf6f130079ee760c0
Jue Wang <j2582wan@uwaterloo.ca>
2022-02-16 19:15:32 -0500

    commit

--- frontEnd/src/components/layout/Header.js
+++ frontEnd/src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Input, Menu, Dropdown, Button } from 'antd';
 import { Link } from 'react-router-dom';
 // import { useLocation, useNavigate, useParams } from 'react-router-dom';
@@ -16,7 +16,7 @@ const menu = (
         </Menu.Item>
         <Menu.Item key="1">
             <Button type="link" block>
-                <Link to="/login" state={{status: 0}}>New customer? Start here.</Link>
+                <Link to="/login" state={{ status: 0 }}>New customer? Start here.</Link>
             </Button>
         </Menu.Item>
     </Menu>
@@ -26,35 +26,45 @@ let username = Cookies.get("user");
 
 export default function Header()  {
 
+    // let [userStatus, setUserStatus] = useState(username);
+
+    const [login, setLogin] = useState(false);
+
+    // setLogin(username);
+
     const onSearch = (value) => {
         console.log(value);
     }
 
     return (
         <div>
-        <Row>
-          <Col flex="150px">WATERLOO</Col>
-          <Col flex={4}>
-            <Search style={{paddingTop: "16px"}} placeholder="Search" allowClear onSearch={onSearch} />
-          </Col>
-          <Col flex={2}>
-            <Row justify="end">
-                <Col span={10}>
-                    {
-                        !username &&
-                        <Dropdown overlay={menu} placement="bottomLeft" arrow >
-                            <span>Hello, Sign in</span>
-                        </Dropdown>
-                    }
-                    {
-                        <span>Hello,</span> && username 
-                    }
-                </Col>
-                <Col span={6}>Orders</Col>
-                <Col span={4}>Cart</Col>
+            <Row>
+            <Col flex="150px">WATERLOO</Col>
+            <Col flex={4}>
+                <Search style={{paddingTop: "16px"}} placeholder="Search" allowClear onSearch={onSearch} />
+            </Col>
+            <Col flex={2}>
+                <Row justify="end">
+                    <Col span={10}>
+                        {/* {
+                            login ?  <Dropdown overlay={menu} placement="bottomLeft" arrow> <span>Hello, Sign in</span> </Dropdown> :
+                            <span>Hello,</span> && username 
+                        } */}
+                        {
+                            !username &&
+                            <Dropdown overlay={menu} placement="bottomLeft" arrow>
+                                <span>Hello, Sign in</span>
+                            </Dropdown>
+                        }
+                        {
+                            <span>Hello,</span> && username 
+                        }
+                    </Col>
+                    <Col span={6}>Orders</Col>
+                    <Col span={4}>Cart</Col>
+                </Row>
+            </Col>
             </Row>
-          </Col>
-        </Row>
         </div>
     );
 }
--- frontEnd/src/pages/Login/Register.js
+++ frontEnd/src/pages/Login/Register.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./index.css"
+
+
 
 import { apiAddUser, apiLoginUser } from '../../request/api';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Tooltip, Space, message} from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import { Row, Col } from 'antd';
 import Cookies from 'js-cookie';
 
@@ -14,6 +17,9 @@ export default function Login() {
   const { state } = useLocation();
   let navigate = useNavigate();
 
+  // let [alertStatus, setAlertStatus] = useState(true);
+
+
   const onFinish = (values) => {
 
     const {username, password} = values;
@@ -21,8 +27,8 @@ export default function Login() {
     if(state.status === 1) {
       //login
       apiLoginUser({username, password}).then((res) => {
-        // console.log('reslogin_______', res);
         if(res.data.flag === true) {
+          message.success('Success login!');
           navigate("/", {state: {username: username}});
           Cookies.set("user", username);
         }
@@ -30,26 +36,60 @@ export default function Login() {
         console.log('err', err)
       })
     } else if (state.status === 0) {
-      // create
-      apiAddUser({username, password}).then((res) => {
-        console.log('rescreate_______', res);
-      }, err => {
-        console.log('err', err)
-      })
+      const isValidUser = checkValidUser(username, password);
+      console.log('isValidUser', isValidUser)
+
+      // setAlertStatus(isValidUser);
+
+      if(isValidUser) {
+        apiAddUser({username, password}).then((res) => {
+          // console.log('rescreate_______', res);
+          message.success('Success created!');
+          navigate("/", {state: {username: username}});
+          Cookies.set("user", username);
+        }, err => {
+          message.error('Error: failed created!');
+          console.log('err', err)
+        })
+      }
     }
-    
+  
   };
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
+  const checkValidUser = (username, password) => {
+
+    /*
+    Secure Password requirements
+    Password must contain at least one digit [0-9].
+    Password must contain at least one lowercase Latin character [a-z].
+    Password must contain at least one uppercase Latin character [A-Z].
+    Password must contain at least one special character like ! @ # & ( ).
+    Password must contain a length of at least 8 characters and a maximum of 20 characters.
+    */
+
+
+    if(username.length < 4 || username.length > 8) {
+      return false;
+    }
+
+    let regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+    let valid = password.match(regularExpression);
+
+    return valid == null ? false : true;
+
+  }
+
+
   return (
     <div>
           <div className="register">
             <Row>
               <Col flex="auto"></Col>
-              <Col flex="350px">
+              <Col flex="400px">
                 <div>
                   <div className="register_header">WATERLOO</div>
                   <div className="register_form">
@@ -63,40 +103,36 @@ export default function Login() {
                       onFinishFailed={onFinishFailed}
 
                     >
-                      <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                      >
-                        <Input />
-                      </Form.Item>
-                      {/* <Form.Item
-                        name="username"
-                        label="E-mail"
-                        rules={[
-                          {
-                            type: 'email',
-                            message: 'The input is not valid E-mail!',
-                          },
-                          {
-                            required: true,
-                            message: 'Please input your E-mail!',
-                          },
-                        ]}
-                      >
-                        <Input />
-                      </Form.Item> */}
-                      <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                      >
-                        <Input.Password />
-                      </Form.Item>
+                      <Space>
+                        <Form.Item
+                          label="Username"
+                          name="username"
+                          rules={[{ required: true, message: 'Please input!' }]}
+                        >
+                          <Input style={{ width: 200 }} />
+                        </Form.Item>
+                      </Space>
+                      <Space>
+                        <Tooltip className="notice_user"  placement="bottomLeft"  title="4 - 8 characters">
+                            <QuestionCircleOutlined />
+                        </Tooltip>
+                     </Space>
 
-                      {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 4, span: 20 }}>
-                        <Checkbox>Remember me</Checkbox>
-                      </Form.Item> */}
+
+                      <Space>
+                        <Form.Item
+                          label="Password"
+                          name="password"
+                          rules={[{ required: true, message: 'Please input!' }]}
+                        >
+                          <Input.Password style={{ width: 200 }} />
+                        </Form.Item>
+                      </Space>
+                      <Space>
+                        <Tooltip className="notice" placement="bottomLeft" title="at least one digit, at least one lowercase Latin character, at least one uppercase Latin character, at least one special character, 8 - 20 characters">
+                          <QuestionCircleOutlined />
+                        </Tooltip>
+                     </Space>
 
                       <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
                         <Button type="primary" htmlType="submit">
@@ -111,6 +147,9 @@ export default function Login() {
               <Col flex="auto"></Col>
             </Row>
           </div>
+          {/* <div>
+            {!alertStatus && <Alert message="Error" type="error" showIcon /> }
+          </div> */}
     </div>
   )
 }
